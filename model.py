9800e4b8303eb2ddf3acb4ad1038bccc816dd43b
anever <a.neverov@g.nsu.ru>
2025-02-05 13:18:43 +0700

    total update

--- model.py
+++ model.py
@@ -1,247 +1,127 @@
 import numpy as np
+import copy as copy
 from dataclasses import dataclass
 
 
 @dataclass
 class Data:
-    keys: set
-    points: list #np.ndarray
+    keys: list
+    points: list  # np.ndarray
     data: np.ndarray
 
-# default_step = 0.01/12
 
-default_step = 0.01/12
+default_step = 1 / 1200
 
-def rungekutta4(func, param, init_x, T, step = default_step, econ_data=None, **kwargs):
 
+def rungekutta4(func, init_x: dict, T: float, step: float = default_step, **kwargs):
     ## input :
     # func(parameters, x_values) - function discribes the model
     # param - system parameters
-    
+
     # init_x - initial system values
     # T - time-end
     # step - grid step
-    
+
     ## output :
     # result - system vaue on grid of time points
-    init_x[5] = param[20]
-    Nt = int(T/step) 
-    ad_shape = int(param.size/param.shape[0])
-    init_val_temp = np.asarray([init_x.copy()]*ad_shape).T
-    
-    result = np.zeros((len(init_x), Nt+1, ad_shape))
-    result[:,0] = init_val_temp
-        
-    for j in range(1,Nt+1):
-    
-        if econ_data:
-            econ = econ_data.iloc[int(np.floor(j*step))]
-            try:
-                param[1] = np.array(param[-len(econ):,0]).dot(np.array(econ))
-            except:
-                param[1] = np.array(param[-len(econ):]).dot(np.array(econ))
-        a1 = step * func(param, init_val_temp, j*step)
-        val_temp = init_val_temp + a1*0.5
+    Nt = int(T / step)
+    # init_val_temp = np.fromiter(init_x.values(), np.float32)
+    init_val_temp = init_x
+    keys = init_x.keys()
+    result = np.zeros((len(init_x), Nt + 1))
+    result[:, 0] = np.fromiter(init_val_temp.values(), np.float32)
 
-        a2 = step * func(param, val_temp, (j+0.5)*step)
-        val_temp = init_val_temp + a2*0.5
-
-        a3 = step * func(param, val_temp, (j+0.5)*step)
-        val_temp = init_val_temp + a3
-
-        a4 = step * func(param, val_temp, (j+1.0)*step)
-
-        init_val_temp += (a1 + 2.0 * a2 + 2.0 * a3 + a4)*1.0 / 6.0
-
-        result[:,j] = init_val_temp
-
+    for j in range(1, Nt + 1):
+        a1 = step * func(system_state=init_val_temp, t=j * step, **kwargs)
+        val_temp = dict(zip(keys, result[:, j - 1] + a1 * 0.5))
+        a2 = step * func(system_state=val_temp, t=(j + 0.5) * step, **kwargs)
+        val_temp = dict(zip(keys, result[:, j - 1] + a2 * 0.5))
+        a3 = step * func(system_state=val_temp, t=(j + 0.5) * step, **kwargs)
+        val_temp = dict(zip(keys, result[:, j - 1] + a3))
+        a4 = step * func(system_state=val_temp, t=(j + 1.0) * step, **kwargs)
+        result[:, j] = result[:, j - 1] + (a1 + 2 * a2 + 2 * a3 + a4) / 6
     return result
 
 
-model_keys = np.array(['S', 'L', 'I', 'T', 'J1', 'J2', 'J3', 'A',
-                       'S+', 'L+', 'I+', 'T+', 'J1+', 'J2+', 'J3+', 'A+'])
-
-def TB_HIV_model(param, sys_x, time=0):
+def ode_model(
+    equation_strings: dict,
+    system_state: dict,
+    params: dict = {},
+    custom_vars: dict = {},
+    t: float = 0,
+):
     ## input :
     # param - system parameters
     # sys_x - system values at indicated time point
-    
+
     ## output :
     # result - system values at indicated time point
-    
+
     ### TB+HIV model ####
-    result = [0]*len(sys_x)
-    R = sys_x[0]+sys_x[1]+sys_x[3]+sys_x[4]+sys_x[5]
-    J_ast = sys_x[5]+sys_x[6]+sys_x[7] #!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    JR = J_ast*1.0/R
-
-    result[0] = param[0] - param[1]*sys_x[0]*(sys_x[2]+sys_x[6])/param[15] - param[2]*sys_x[0]*JR - param[3]*sys_x[0]
-    result[1] = param[1]*(sys_x[0]+sys_x[3])*(sys_x[2]+sys_x[6])/param[15] - param[2]*sys_x[1]*JR - (param[3]+param[4]+param[9])*sys_x[1]
-    result[2] = param[4]*sys_x[1] -(param[3]+param[6]+param[10])*sys_x[2]
-    result[3] = param[9]*sys_x[1] + param[10]*sys_x[2] - param[1]*sys_x[3]*(sys_x[2]+sys_x[6])/param[15] - param[2]*sys_x[3]*JR - param[3]*sys_x[3]
-    result[4] = param[2]*(sys_x[0]+sys_x[3])*JR - param[1]*sys_x[4]*(sys_x[2]+sys_x[6])/param[15] - (param[12]+param[3])*sys_x[4] + param[21]*sys_x[6]
-    result[5] = param[2]*sys_x[1]*JR + param[1]*sys_x[4]*(sys_x[2]+sys_x[6])/param[15] + param[11]*sys_x[6] - (param[13]+param[3]+param[5])*sys_x[5]
-    result[6] = param[5]*sys_x[5] - (param[14]+param[3]+param[7]+param[11])*sys_x[6] - param[21]*sys_x[6]
-    result[7] = param[12]*sys_x[4] + param[13]*sys_x[5] + param[14]*sys_x[6] - (param[3]+param[8])*sys_x[7]
-
-    return np.array(result)
-
-def TB_HIV_plus(param, sys_x, time=0):
-    ## input :
-    # param - system parameters
-    # sys_x - system values at indicated time point
-    
-    ## output :
-    # result - system values at indicated time point
-    
-    ### TB+HIV model ####
-    result = [0]*len(sys_x)
-    # result = np.empty((len(sys_x)))
-    R = sys_x[0]+sys_x[1]+sys_x[3]+sys_x[4]+sys_x[5]
-    J_ast = sys_x[5]+sys_x[6]+sys_x[7] #!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    JR = J_ast*1.0/R
-    # param = np.array(param)
-    # sys_x = np.array(sys_x)
-
-
-    result[0] = param[0] + sys_x[0]-sys_x[0] #- param[1]*sys_x[0]*(sys_x[2]+sys_x[6])/param[15] - param[2]*sys_x[0]*JR - param[3]*sys_x[0]
-    result[1] = param[1]*(sys_x[0]+sys_x[3])*(sys_x[2]+sys_x[6])/param[15]
-    result[2] = param[4]*sys_x[1]
-    result[3] = param[9]*sys_x[1] + param[10]*sys_x[2] 
-    result[4] = param[2]*(sys_x[0]+sys_x[3])*JR
-    result[5] = param[2]*sys_x[1]*JR + param[1]*sys_x[4]*(sys_x[2]+sys_x[6])/param[15] + param[11]*sys_x[6] 
-    result[6] = param[5]*sys_x[5]
-    result[7] = param[12]*sys_x[4] + param[13]*sys_x[5] + param[14]*sys_x[6]
+    _custom_vars = copy.copy(custom_vars)
+    aliases_dict = params | system_state | {"t": t}
+    for key in _custom_vars.keys():
+        _custom_vars[key] = eval(_custom_vars[key], aliases_dict)
+    result = np.zeros(len(equation_strings))
+    for i, val in enumerate(equation_strings.values()):
+        result[i] = eval(val, aliases_dict | _custom_vars)
     return np.array(result)
 
 
-def calculate_plus(params, data, points, T, n_equations, steps = 10):
-    try:
-        data_plus = np.zeros((data.shape[0], data.shape[1], params.shape[1]))
-    except:
-        data_plus = np.zeros((data.shape[0], data.shape[1],1))
-    #print(params.shape)
-    points_plus = points
-    # print(data.shape)
-    for i in range(data.shape[1]):
-        for j in range(steps):
-            # print(params.shape)
-            # print(TB_HIV_plus(params, data[:,i-j]).shape)
-            # print(data_plus[:,i].shape)
-
-            data_plus[:,i] += TB_HIV_plus(params, data[:,i-j])
-        # if i>0:
-        # points_plus[i] = points_plus[i-1]+step
-    data_plus *= default_step
-    return data_plus, points_plus
-
-def TB_rk4(params, init_x, T, **kwargs):
-    data = np.array(rungekutta4(TB_HIV_model, params, init_x, T, **kwargs))
-    points = np.array([np.linspace(0,T,num = len(data[0]), endpoint=True) for i in range(len(data))])
-
-    data_plus, points_plus = calculate_plus(params, data, points, n_equations=data.shape[0] , T=T)
-
-    data = np.concatenate((data, data_plus))
-    points = np.concatenate((points, points_plus))
-    keys = model_keys
-
+def ode_rk4(params, init_x, T, t0=0, **kwargs):
+    data = np.array(
+        rungekutta4(func=ode_model, params=params, init_x=init_x, T=T - t0, **kwargs)
+    )
+    points = np.array([
+        np.linspace(t0, T, num=len(data[0]), endpoint=True) for _ in range(len(data))
+    ])
+    keys = np.array(list(init_x.keys()))
     return Data(keys, points, data)
-    
 
-def data_prediction(nb_mes, mes_eq_ind, **kwargs): # model,param, init_x, T, ):
-    ## input :
-    # nb_mes - number of measurements
-    # mes_eq_ind - indexes of measured equations
-
-    ## output :
-    # result - predicted data (for parameters) at indicated time points, except 0-point
-    
-    result = TB_rk4(**kwargs).data #(model, param, init_x, T)
-    nt = result.shape[1]
-    
-    ### to return only the mesurements at indecated time points, without 0-point
-    mesurements = np.arange(0, nt, int((nt-1)*1.0/nb_mes))[1:]
-    
-    return result[mes_eq_ind][:,mesurements]
-
-def syntetic_data(noise = [[0.2], [0], [1]], **kwargs):
-    ## input :
-    # noise = [%s, alfas, sigmas] - percentages of given gaussian noises for each measured equations
-    
-    ## output :
-    # result - synthetic data with given noise at indicated time points, except 0-point
-    
-    result = data_prediction(**kwargs)
-    nt = result.shape
-    
-    if (noise != None):
-        result += noise[0]*np.random.normal(noise[1],noise[2],nt)
-    
-    ### to remove some mistakes with negative numbers
-    condition = result < 0.0
-    result[condition] = 0.0
-     
-    return result
 
 def loss_func(data_pred: Data, data_ex: Data):
     ## input :
     # data_pred - predicted data
     # data_ex - exact data
-    
+
     ## output :
     # quadratic loss
     target_sum = 0
-
-    _f = lambda x: x in data_ex.keys
-    inds = list(map(_f, data_pred.keys))
+    inds = list(map(lambda x: x in data_ex.keys, data_pred.keys))
     data_pred.data = data_pred.data[inds]
     data_pred.points = data_pred.points[inds]
     data_pred.keys = data_pred.keys[inds]
 
     if np.all(data_pred.keys != data_ex.keys):
-        raise ValueError('Given exact data keys do not match model keys')
-    # temp = [0]*(len(data_pred.keys))
+        raise ValueError("Given exact data keys do not match model keys")
 
     for i in range(len(data_pred.keys)):
-        _f = lambda x: (x in data_ex.points[i])
-        _f2 = lambda x: (x in data_pred.points[i])
-        inds = list(map(_f, data_pred.points[i]))
-        inds2 = list(map(_f2, data_ex.points[i]))
-        
-        # temp[i] = data_pred.points[i, inds]
-        # print(data_pred.data[i, inds].T.shape, '    ', np.array(data_ex.data[i]).shape)
-        # print(data_pred.data[i, inds].T, '   ', np.array(data_ex.data[i])[inds2])
+        inds = list(map(lambda x: (x in data_ex.points[i]), data_pred.points[i]))
+        inds2 = list(map(lambda x: (x in data_pred.points[i]), data_ex.points[i]))
+        print(data_ex.points)
+
         dp = np.array(data_ex.data[i])[inds2]
-        #dp_n = np.sqrt(np.sum(dp*dp))
-        dp_n = np.sum(np.abs(dp)**2)
-        target_sum += np.sum((data_pred.data[i, inds].T - dp).T**2, (0))/dp_n   
-
+        # dp_n = np.sqrt(np.sum(dp*dp))
+        dp_n = np.sum(np.abs(dp) ** 2)
+        target_sum += np.sum((data_pred.data[i, inds].T - dp).T ** 2) / dp_n
     return target_sum
-    #return np.sum((data_pred.data - data_ex.data)**2, (0,1))
+    # return np.sum((data_pred.data - data_ex.data)**2, (0,1))
 
 
-def full_func(data_ex: Data, model, **kwargs): # init_x, T, nb_mes, mes_eq_ind):
+def objective(data_ex: Data, model, default_params, trial_params, **kwargs):
     ## input :
     # data_ex - exact data
 
     ## output :
     # quadratic loss of model with indicated parameters and exact data
-    
-    prediction = model(**kwargs)#data_prediction(**kwargs) #param, model, init_x, T, nb_mes, mes_eq_ind)
-    normed_pred = prediction # prediction/np.asarray([np.max(prediction, prediction.shape[-1]).T]).T
-    normed_ex = data_ex # data_ex/np.asarray([np.max(data_ex, data_ex.shape[-1]).T]).T
-    
+
+    params = copy.copy(default_params)
+    params.update(trial_params)
+    prediction = model(params=params, **kwargs)
+    normed_pred = prediction  # prediction/np.asarray([np.max(prediction, prediction.shape[-1]).T]).T
+    normed_ex = data_ex  # data_ex/np.asarray([np.max(data_ex, data_ex.shape[-1]).T]).T
     return loss_func(normed_pred, normed_ex)
 
 
-def fill_param(x, param_ind_, exact_param):
-    added_shape = int(x.size/x.shape[0])
-    param = np.asarray([exact_param.copy()]*added_shape).T
-    
-    if added_shape == 1 :
-        param[param_ind_] = x.reshape((-1, 1))
-      
-    else: 
-        param[param_ind_] = x
-
-    return param
+def ode_objective(**kwargs):
+    return objective(model=ode_rk4, **kwargs)
--- runner.py
+++ runner.py
@@ -3,198 +3,240 @@ import yaml
 import matplotlib.pyplot as plt
 import pandas as pd
 from copy import deepcopy as copy
-from model import TB_HIV_model, rungekutta4, TB_rk4, Data, fill_param, full_func
-from optuna_optimiser import run_optuna, objective
-#from sklearn import preprocessing
-#import scipy
-import time
-#from numpy import linalg as LA
+from model import TB_HIV_model, rungekutta4, TB_rk4, Data, objective, ode_objective
+from optuna_optimiser import run_optuna
 
-with open('paramteres.yml', 'r', encoding='utf8') as file:
+import sqlite3
+
+with open("paramteres.yml", "r", encoding="utf8") as file:
     p = yaml.safe_load(file)
-#Y0 = np.array(list(p['initial_state'].values()))
-P_names = list(p['all_parameters'].keys())
-P_exact = np.array(list(p['all_parameters'].values()))
-param_ind = np.array(list(p['indeces2estim'].values()), dtype=int)
-Nb_estim_param = len(param_ind)
-
-print(Nb_estim_param)
-
-## Boundaries of the parameter definition domains
-min_bounds = np.zeros(Nb_estim_param)
-max_bounds = np.ones(Nb_estim_param)*4.01 ### 9, 10, 11 may be from 0 to inf
-max_bounds[:7] = np.array([100,5,2,2,2,2,2])
-min_bounds[2] = 0.05
-min_bounds[5:7] = np.array([0.5,0.5])
-
-max_bounds[-2]=100. #J2 init
-min_bounds[-2]=0.
-
-full_df = pd.read_csv('epid.csv')
-
-def output(string):
-    with open('output.txt', 'a') as file:
-        file.write(string+'\n')
-
-def region_df(reg_name = '', df = full_df, df_e = None):
-    _df = df[df['регион']==reg_name]
-    _df.index = _df['год']
-    _df = _df.drop(['год','регион'], axis = 1)
-    _df = _df[['I+J3','J3','J1','N']]
-    
-    return _df
 
 
+# def output(string: str):
+#    with open("output.txt", "a") as file:
+#        file.write(string + "\n")
 
-regions = np.unique(full_df['регион'])
 
-#cycle through "regions" to estimate parameters for all regions or place list of needed region names
-for region in ['Свердловская обл.', 'Московская обл.']: # for region in ['regions']:
-    try:
-        output(region)
-        df_region = region_df(region)
-        
-        k=12
-        n=(12)*k
-        T = k-1 + 3
-        
-        print(df_region)
-        pop = df_region['N'].iloc[0]
-        P_exact[15] = pop
-        print(pop)
-        model_kwargs = {
-            'model': TB_rk4,
-            'init_x': None,
-            'T': None,
-            'data_ex': None,
+db_filename = "tuberculosis.db"
 
-            'param_ind': param_ind,
-            'min_bounds':min_bounds, 
-            'max_bounds':max_bounds,
-            'fill_param':lambda params: fill_param(params.T, param_ind_ = param_ind, exact_param = P_exact),
-        }
-        
-        I = df_region['I+J3']/1000 
-        J1 = df_region['J1']/1000
-        J3 = df_region['J1']/1000
-        
-        I = I-J3
-        
-        #S L I T J1 J2 J3 A
-        Y0 = np.array([0.95,0.05,0,0,0,0,0,0]) * pop
-        Y0[2] = I.iloc[0]
-        Y0[3] = J1.iloc[0]
-        Y0[4] = J1.iloc[0]
-        Y0[5] = J1.iloc[0]*0.5
-        Y0[6] = J3.iloc[0]
-        
-        print(Y0)
-        model_kwargs['init_x'] = Y0
-        
-        for i in range(int(12/k)):
-            start_point = 0
-            end_point = k+start_point
-            
-            
-            keys = ['I', 'J1']#, 'J3'
-            real_data = np.array([I, J1]) #, J3
-            eq_ind = np.array([2,4], #,6
-                                dtype=int)
+connection = sqlite3.connect(db_filename)
+cursor = connection.cursor()
+cursor.execute("SELECT * from 'Reg'")
+all_regions = cursor.fetchall()
+region = all_regions[5]
+# print(all_regions)
+cursor.execute(
+    "SELECT Year, ParameterID, value from Data WHERE RegionID = " + str(region[0])
+)
+reg_data = cursor.fetchall()
 
-            points = ([list(range(k))]*len(real_data))
-            
-            rest_points = ([list(range(k, len(I)))] * len(real_data))
-            
-            syn_data = Data(keys=keys, points = points, data = real_data[:,start_point:end_point])
-            
-            model_kwargs['T'] = T
-            model_kwargs['data_ex'] = syn_data
-            
-            print(model_kwargs)
-            print('Optuna run')
-            optuna_dict, best_val = run_optuna(**model_kwargs)
-            
-            _optuna_P = np.array(list(optuna_dict.values()))
-            optuna_P = P_exact.copy()
-            optuna_P[param_ind] = _optuna_P
-            
-            print('Start plotting')
+cursor.execute("SELECT * from Par")
+all_pars = dict(cursor.fetchall())
 
-            optuna = rungekutta4(TB_HIV_model, optuna_P, Y0, T,)
-            
-            optuna_Pb = copy(optuna_P)
-            optuna_Pw = copy(optuna_P)
-            
-            optuna_Pb[10] *= 1.1
-            optuna_Pw[10] *= 0.9
-            
-            optuna_better_treat = rungekutta4(TB_HIV_model, optuna_Pb, Y0, T,)
-            
-            optuna_worse_treat = rungekutta4(TB_HIV_model, optuna_Pw, Y0, T,)
+df_region = pd.DataFrame(columns=list(all_pars.values()))
+for date, parameterID, value in reg_data:
+    # df_region.loc[date, all_pars[parameterID]] = value
+    df_region.loc[date, parameterID] = value
 
-            finish_state = optuna[:,-1].T[0]
-            
-            model_kwargs['init_x'] = finish_state
-            Y0 = finish_state
+params_from_db = list(p["db_keys"].values())
+df_region = df_region[params_from_db]
+df_region.columns = list(p["db_keys"].keys())
 
-            titles = [ 'Number of TB infectious (without HIV) individuals (I)',
-                      'Number of infectious with HIV (J1)',
-                      'Number of infectious with both TB and HIV individuals (J3)',
-                      ]
-            
-            
-            short_titles = [
-                      '(I)',
-                      '(J1)',
-                      '(J3)']
-                      
-            syn_data = syn_data.data
-            year_step = 1
-            st_year = 2009+start_point
-            end_year = st_year+int(np.floor(T)+1)
-            for g in range(len(eq_ind)):
-                plt.figure(figsize=(10,6))
-                i,j,_ = optuna.shape
-                
-                plt.plot(np.arange(j), optuna[eq_ind[g]],label = 'Modelling result', linewidth = 3, linestyle = 'dashed', color = '#335df5')
-                plt.plot(np.arange(j), optuna_better_treat[eq_ind[g]],label = 'Better treatment', linewidth = 3, linestyle = 'dashed', color = '#5cd424')
-                plt.plot(np.arange(j), optuna_worse_treat[eq_ind[g]],label = 'Worse treatment', linewidth = 3, linestyle = 'dashed', color = '#f56d33')
-                
-                try:
-                    new_p = np.array(points[0])* int(j/T)
-                    plt.scatter(new_p , real_data[g][start_point:start_point+len(new_p)], label = 'Train data', linewidth =4, color='black')
-                    new_p2 = np.array(rest_points[0])* int(j/T)
-                    plt.scatter(new_p2 , real_data[g][start_point+len(new_p):start_point+len(new_p)+len(new_p2)], label = 'Test data', linewidth =4, color='red')
-                except:
-                    pass
-                plt.legend(fontsize=12)
-                
-                plt.xticks(np.arange(0, j+1, int(np.floor(year_step*j/T))),
-                           np.arange(st_year, end_year, year_step, dtype=int), 
-                           color='black',fontsize=14)
-                plt.yticks(fontsize=18)
-                plt.title(titles[g],fontsize=18)
-                plt.savefig(short_titles[g]+region+str(st_year)+'-'+str(end_year-1)+'.png', dpi=300)
-                # plt.show()
-            output(str(st_year)+'-'+str(end_year-1))
-            #output('best result '+str(best_val))
-            output(str(optuna_dict)+'\n')
-    
-    
-    
-        print('Plotted')
-        mu = P_exact[3]
-        d = P_exact[6]
-        o1 = mu + optuna_dict['4'] + optuna_dict['9']
-        o2 = mu + d + optuna_dict['10']
-        print('R1')
-        print(optuna_dict['1'] * optuna_dict['4']/o1/o2)
-        
-        print('R2')
-        print(optuna_dict['2'] / (mu + 0.1))
-        
-        for i in range(len(optuna_P)):
-            print(P_names[i], ' : ', optuna_P[i])
-    except:
-        output('region_failed\n')
-    
+df_region["I"] = df_region["I+J3"] - df_region["J3"]
+
+connection.close()
+
+points = []
+values = []
+# make here keys to be passed to inverse problem, but not all the data in db
+for key in p["passed_keys"]:  # df_region.columns:
+    temp_df = df_region[key].dropna()
+    points.append(list(temp_df.index))
+    values.append(list(temp_df))
+inverse_problem_data = Data(keys=p["passed_keys"], points=points, data=values)
+initial_state = p["initial_state"]
+
+params = p["all_parameters"]
+P_names = list(params.keys())
+P_exact = np.array(list(params.values()))
+estimation_bounds = p["estim_and_bounds"]
+for key in estimation_bounds.keys():
+    if estimation_bounds[key] == "default":
+        estimation_bounds[key] = np.array([0, 1])
+
+t0 = 2009
+T = t0 + 10
+
+pop = df_region["N"].iloc[0]
+P_exact[15] = pop * 1000
+
+for key in initial_state.keys():
+    initial_state[key] *= pop
+
+model_kwargs = {
+    "model": TB_rk4,
+    "init_x": initial_state,
+    "t0": 2009,
+    "T": T,
+    "data_ex": inverse_problem_data,
+    "default_params": params,
+    "objective": objective,
+    "estimation_bounds": estimation_bounds,
+}
+
+
+model_kwargs_new = {
+    "init_x": initial_state,
+    "t0": 2009,
+    "T": T,
+    "data_ex": inverse_problem_data,
+    "default_params": params,
+    "objective": ode_objective,
+    "estimation_bounds": estimation_bounds,
+    "equation_strings": p["equations"],
+    "custom_vars": p["custom_vars"],
+}
+
+# regions = np.unique(full_df["регион"])
+
+# cycle through "regions" to estimate parameters for all regions or place list of needed region names
+# for region in ["Свердловская обл.", "Московская обл."]:  # for region in ['regions']:
+try:
+    print("Optuna run")
+    optuna_dict, best_val = run_optuna(**model_kwargs_new)
+
+    # _optuna_P = np.array(list(optuna_dict.values()))
+    optuna_P = P_exact.copy()
+    for key in estimation_bounds.keys():
+        optuna_P[key] = optuna_dict[key]
+
+    print("Start plotting")
+
+    optuna = rungekutta4(
+        TB_HIV_model,
+        optuna_P,
+        initial_state,
+        T,
+    )
+
+    optuna_Pb = copy(optuna_P)
+    optuna_Pw = copy(optuna_P)
+
+    optuna_Pb[10] *= 1.1
+    optuna_Pw[10] *= 0.9
+
+    optuna_better_treat = rungekutta4(
+        TB_HIV_model,
+        optuna_Pb,
+        initial_state,
+        T,
+    )
+
+    optuna_worse_treat = rungekutta4(
+        TB_HIV_model,
+        optuna_Pw,
+        initial_state,
+        T,
+    )
+
+    finish_state = optuna[:, -1].T[0]
+
+    model_kwargs["init_x"] = finish_state
+    Y0 = finish_state
+
+    titles = [
+        "Number of TB infectious (without HIV) individuals (I)",
+        "Number of infectious with HIV (J1)",
+        "Number of infectious with both TB and HIV individuals (J3)",
+    ]
+
+    short_titles = ["(I)", "(J1)", "(J3)"]
+
+    syn_data = syn_data.data
+    year_step = 1
+    st_year = 2009 + start_point
+    end_year = st_year + int(np.floor(T) + 1)
+    for g in range(len(eq_ind)):
+        plt.figure(figsize=(10, 6))
+        i, j, _ = optuna.shape
+
+        plt.plot(
+            np.arange(j),
+            optuna[eq_ind[g]],
+            label="Modelling result",
+            linewidth=3,
+            linestyle="dashed",
+            color="#335df5",
+        )
+        plt.plot(
+            np.arange(j),
+            optuna_better_treat[eq_ind[g]],
+            label="Better treatment",
+            linewidth=3,
+            linestyle="dashed",
+            color="#5cd424",
+        )
+        plt.plot(
+            np.arange(j),
+            optuna_worse_treat[eq_ind[g]],
+            label="Worse treatment",
+            linewidth=3,
+            linestyle="dashed",
+            color="#f56d33",
+        )
+
+        try:
+            new_p = np.array(points[0]) * int(j / T)
+            plt.scatter(
+                new_p,
+                real_data[g][start_point : start_point + len(new_p)],
+                label="Train data",
+                linewidth=4,
+                color="black",
+            )
+            new_p2 = np.array(rest_points[0]) * int(j / T)
+            plt.scatter(
+                new_p2,
+                real_data[g][
+                    start_point + len(new_p) : start_point + len(new_p) + len(new_p2)
+                ],
+                label="Test data",
+                linewidth=4,
+                color="red",
+            )
+        except:
+            pass
+        plt.legend(fontsize=12)
+
+        plt.xticks(
+            np.arange(0, j + 1, int(np.floor(year_step * j / T))),
+            np.arange(st_year, end_year, year_step, dtype=int),
+            color="black",
+            fontsize=14,
+        )
+        plt.yticks(fontsize=18)
+        plt.title(titles[g], fontsize=18)
+        plt.savefig(
+            short_titles[g] + region + str(st_year) + "-" + str(end_year - 1) + ".png",
+            dpi=300,
+        )
+        # plt.show()
+    output(str(st_year) + "-" + str(end_year - 1))
+    # output('best result '+str(best_val))
+    output(str(optuna_dict) + "\n")
+
+    print("Plotted")
+    mu = P_exact[3]
+    d = P_exact[6]
+    o1 = mu + optuna_dict["4"] + optuna_dict["9"]
+    o2 = mu + d + optuna_dict["10"]
+    print("R1")
+    print(optuna_dict["1"] * optuna_dict["4"] / o1 / o2)
+
+    print("R2")
+    print(optuna_dict["2"] / (mu + 0.1))
+
+    for i in range(len(optuna_P)):
+        print(P_names[i], " : ", optuna_P[i])
+except:
+    output("region_failed\n")
